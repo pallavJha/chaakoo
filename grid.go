3d2b0efb1aeaaf1aa7a1c6c99eed1256e98c7655
pallavJha <pallav_kumar71@yahoo.com>
2021-09-12 00:40:48 +0530

    start a dfs logic

--- grid.go
+++ grid.go
@@ -56,69 +56,116 @@ type pane struct {
 	visited bool
 	left    *pane
 	botton  *pane
-    parent *pane
+	parent  *pane
+}
+
+func (p *pane) Height() int {
+	return p.yEnd - p.yStart + 1
+}
+
+func (p *pane) Width() int {
+	return p.xEnd - p.xStart + 1
 }
 
 func prepareGraph(grid [][]string) {
-    panes, err := preparePanes(grid)
-    if err != nil {
-        // handle error
-    }
+	panes, err := preparePanes(grid)
+	if err != nil {
+		// handle error
+	}
 
-    var nameToPanes = make(map[string]*pane)
-    var visited = make(map[string]bool)
-    for _, pane := range panes {
-        nameToPanes[pane.name] = pane
-        visited[pane.name] = false
-    }
+	var nameToPanes = make(map[string]*pane)
+	var visited = make(map[string]bool)
+	for _, pane := range panes {
+		nameToPanes[pane.name] = pane
+		visited[pane.name] = false
+	}
+
+  numberOfPanes := len(panes)
+  for numberOfPanes > 0 {
+    dfs(panes[0], grid, nameToPanes, visited)
+    numberOfPanes--
+  }
+
 }
 
-func dfs(currentPane *pane, grid [][]string, panes map[string]*pane, visited map[string]bool) {
-   leftPaneName := getLeftPaneName(currentPane, grid, panes)
-   var leftPane *pane
-   if len(leftPaneName) > 0 {
-        leftPane = panes[leftPaneName]
-   }
+func dfs(currentPane *pane, grid [][]string, panes map[string]*pane, visited map[string]bool) *pane {
+	leftPaneName := getLeftPaneName(currentPane, grid, panes)
+	var leftPane *pane
+	if len(leftPaneName) > 0 {
+		leftPane = panes[leftPaneName]
+		leftPane = dfs(leftPane, grid, panes, visited)
+	}
 
-   rightPaneName := getBottomPaneName(currentPane, grid, panes)
-   var rightPane *pane
-   if len(rightPane) > 0 {
-    rightPane = panes[rightPaneName]
-   }
+	bottomPaneName := getBottomPaneName(currentPane, grid, panes)
+	var bottomPane *pane
+	if len(bottomPaneName) > 0 {
+		bottomPane = panes[bottomPaneName]
+		bottomPane = dfs(bottomPane, grid, panes, visited)
+	}
 
-
-  
+	if leftPane != nil && bottomPane == nil {
+		if leftPane.Height() == currentPane.Height() {
+			// directly add the left pane as they have the same height
+			currentPane.left = leftPane
+			currentPane.xEnd = leftPane.xEnd
+			leftPane.visited = true
+			visited[leftPaneName] = true
+		}
+	} else if leftPane == nil && bottomPane != nil {
+		if bottomPane.Width() == currentPane.Width() {
+			// directly add the right pane as they have the same width
+			currentPane.botton = bottomPane
+			currentPane.yEnd = bottomPane.yEnd
+			bottomPane.visited = true
+			visited[bottomPaneName] = true
+		}
+	} else if leftPane != nil && bottomPane != nil {
+		if leftPane.Height() == currentPane.Height() {
+			// directly add the left pane as they have the same height
+			currentPane.left = leftPane
+			currentPane.xEnd = leftPane.xEnd
+			leftPane.visited = true
+		} else if leftPane.Height() > currentPane.Height() {
+			// here the left pane has more height than the currentPane
+			// so we join the bottom pane to the current pane to increase
+			// the height and
+			currentPane.botton = bottomPane
+			currentPane.yEnd = bottomPane.yEnd
+			bottomPane.visited = true
+			// then join the left pane
+			currentPane.left = leftPane
+			currentPane.xEnd = leftPane.xEnd
+			leftPane.visited = true
+		}
+	}
+	return currentPane
 }
-
 
 func getLeftPaneName(pane *pane, grid [][]string, panes map[string]*pane) string {
-    i, j := pane.yStart, pane.xEnd + 1
-    if j >= len(grid[0]) {
-       return ""
-    }
-    leftPaneName := grid[i][j]
-    leftPane := panes[leftPaneName]
-    if leftPane.yStart == pane.yStart {
-        return leftPaneName
-    }
-    return ""
+	i, j := pane.yStart, pane.xEnd+1
+	if j >= len(grid[0]) {
+		return ""
+	}
+	leftPaneName := grid[i][j]
+	leftPane := panes[leftPaneName]
+	if leftPane.yStart == pane.yStart {
+		return leftPaneName
+	}
+	return ""
 }
-
 
 func getBottomPaneName(pane *pane, grid [][]string, panes map[string]*pane) string {
-    i, j := pane.yEnd + 1, pane.xStart
-    if i >= len(grid) {
-       return ""
-    }
-    bottomPaneName := grid[i][j]
-    bottomPane := panes[bottomPaneName]
-    if bottomPane.xStart == pane.xStart {
-        return bottomPaneName
-    }
-    return ""
+	i, j := pane.yEnd+1, pane.xStart
+	if i >= len(grid) {
+		return ""
+	}
+	bottomPaneName := grid[i][j]
+	bottomPane := panes[bottomPaneName]
+	if bottomPane.xStart == pane.xStart {
+		return bottomPaneName
+	}
+	return ""
 }
-
-
 
 func preparePanes(grid [][]string) ([]*pane, error) {
 	var visited [][]bool = make([][]bool, len(grid))
