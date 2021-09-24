a8975f76d3f4d001ca9b83a9e0633d0f74dbc835
pallavJha <pallav_kumar71@yahoo.com>
2021-09-24 22:42:36 +0530

    add testcases using mock command executor

--- tmux_wrapper_test.go
+++ tmux_wrapper_test.go
@@ -1,39 +1,66 @@
 package chaakoo
 
 import (
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/pallavJha/chaakoo/mocks"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
 type TmuxWrapperTestCase struct {
-	Id          int
+	ID          int
 	Dimension   *Dimension
 	SessionName string
 	Windows     []*Window
-	Commands    []struct {
-		Name string
-		Args string
+	Commands    []*struct {
+		Name     string
+		Args     string
+		Stdout   string
+		Stderr   string
+		Err      string
+		ExitCode int
 	}
 }
 
 func (c TmuxWrapperTestSuite) testTmuxWrapperApply(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	var testCases []TmuxWrapperTestCase
 	if err := viper.UnmarshalKey("configs", &testCases); err != nil {
 		t.Log("unable to read from the config", err)
 		t.Fail()
 	}
 	for _, testCase := range testCases {
-		t.Log("testing, id", testCase.Id)
+		t.Log("testing, id", testCase.ID)
 		config := &Config{
 			SessionName: testCase.SessionName,
 			Windows:     testCase.Windows,
+			DryRun:      true,
 		}
 		err := config.Validate()
 		require.NoError(t, err)
 		err = config.Parse()
 		require.NoError(t, err)
 		wrapper := NewTmuxWrapper(config, testCase.Dimension)
+
+		mockCmdExecutor := mocks.NewMockICommandExecutor(ctrl)
+		wrapper.executor = mockCmdExecutor
+		for _, command := range testCase.Commands {
+			command.Args = strings.TrimSpace(command.Args)
+			arguments := strings.Split(command.Args, " ")
+			var errorToReturn error
+			if len(command.Err) > 0 {
+				errorToReturn = errors.New(command.Err)
+			}
+			mockCmdExecutor.EXPECT().Execute(command.Name, arguments).Return(
+				command.Stdout, command.Stderr, command.ExitCode, errorToReturn,
+			)
+		}
+
 		err = wrapper.Apply()
 		require.NoError(t, err)
 	}
